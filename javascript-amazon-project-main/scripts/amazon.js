087f736954342152117bc1862d66904a93beeb3f
harishvenkateshan <harishvenkateshan@gmail.com>
2025-10-26 11:54:16 +0530

    used product.js file

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,32 +1,40 @@
-const products = [
-  {
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',         //1. we creating sample hard code js-product-grid products object into list
-    rating: {
-      stars: 4.5,
-      count: 87
-    },
-    priceCents: 1090
+// const products = [
+//   {
+//     image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+//     name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',         //1. we creating sample hard code js-product-grid products object into list
+//     rating: {
+//       stars: 4.5,
+//       count: 87
+//     },
+//     priceCents: 1090
 
-  },
-  {
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating:{
-      stars: 4,
-      count: 127
-    },
-    priceCents: 2095
-  },{
-    image:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-      stars: 4.5,
-      count: 56
-    },
-    priceCents: 799
-  }
-]
+//   },
+//   {
+//     image: 'images/products/intermediate-composite-basketball.jpg',
+//     name: 'Intermediate Size Basketball',
+//     rating:{
+//       stars: 4,
+//       count: 127
+//     },
+//     priceCents: 2095
+//   },{
+//     image:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+//     name: 'Adults Plain Cotton T-Shirt - 2 Pack',
+//     rating: {
+//       stars: 4.5,
+//       count: 56
+//     },
+//     priceCents: 799
+//   },{
+//     image: "images/products/6-piece-white-dinner-plate-set.jpg",
+//     name: "6 Piece White Dinner Plate Set",
+//     rating:{
+//       stars: 2,
+//       count: 55
+//     },
+//     priceCents: 2067
+//   }
+// ]
 
 let productsHTML = '';                                // we are using acumalator pattern here its just creating a variable to store a value ffor late use after looping thr an array
 
