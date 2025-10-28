cf0002f652e1d20c3ce3f363d2dfd26db3776480
harishvenkateshan <harishvenkateshan@gmail.com>
2025-10-28 11:21:00 +0530

    moved addToCart and upateCartQuantity into cart.js

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,4 +1,36 @@
 export const cart = []
 
 
+export function addToCart(productId){
+    // check item already in cart or not
+    let matchingItem;    // returns truthy/Falsy value
+
+    cart.forEach((CartItem)=>{        // looping thr an arrow to check item already there or not
+      if(productId === CartItem.productId){     // if item there in cart list as an object
+        matchingItem = CartItem                 // store the matching item in seprate variable
+      }
+    });
+
+    const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`)
+    const quantity = Number(quantitySelector.value)
+
+    if (matchingItem){             // if matchingItem is truthy
+      matchingItem.quantity += quantity     // increase the matching product quuantity +1
+    } else {                       // else push the product object(productId , quantity) in cart array
+      cart.push({
+        productId,         //productId: productId,
+        quantity             // quantity: quantity
+      })
+    }
+}
+
+export function updateCartQuantity(){
+ let cartQuantity = 0
+    cart.forEach((CartItem)=>{
+      cartQuantity += CartItem.quantity
+    })
+      
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity
+}
+
 
--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,4 +1,5 @@
-import {cart} from '../data/cart.js'
+// import * as cartModule from 'file path'  eg. cartModule.cart, cartModule.addToCart
+import {cart, addToCart, updateCartQuantity} from '../data/cart.js'
 import { products } from '../data/products.js';
 
 let productsHTML = '';                                // we are using acumalator pattern here its just creating a variable to store a value ffor later use after looping thr an array
@@ -61,15 +62,9 @@ products.forEach((product,index)=>{                    // we are loop thr an arr
 document.querySelector('.js-products-grid')    // using DOM to get html element by elementsclass name and changing the innnrHTML property with our generated sample code
  .innerHTML = productsHTML
 
- const addedMessageTimeouts = {};
 
- document.querySelectorAll('.js-add-to-cart')
-  .forEach((button)=>{
-    button.addEventListener('click',()=>{
-
-      const {productId} = button.dataset;
-
-      document.querySelector(`.js-added-to-cart-${productId}`).classList.add('after-added-to-cart')
+ function addedMessagedissappair(productId){
+      const addedMessageTimeouts = {};
 
       const previusTimeoutId = addedMessageTimeouts[productId]
 
@@ -82,41 +77,25 @@ document.querySelector('.js-products-grid')    // using DOM to get html element 
       },2000)
 
       addedMessageTimeouts[productId] = timeoutId
+ }
+
+ 
+
+ document.querySelectorAll('.js-add-to-cart')
+  .forEach((button)=>{
+    button.addEventListener('click',()=>{
+
+      const {productId} = button.dataset;
+
+      addToCart(productId)
+
+      updateCartQuantity()
+
+      document.querySelector(`.js-added-to-cart-${productId}`).classList.add('after-added-to-cart')
+
+      addedMessagedissappair(productId)
 
 
-
-
-
-
-      // check item already in cart or not
-      let matchingItem;    // returns truthy/Falsy value
-
-      cart.forEach((item)=>{        // looping thr an arrow to check item already there or not
-        if(productId === item.productId){     // if item there in cart list as an object
-          matchingItem = item                  // store the matching item in seprate variable
-        }
-      });
-
-      const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`)
-
-      const quantity = Number(quantitySelector.value)
-
-      if (matchingItem){             // if matchingItem is truthy
-        matchingItem.quantity += quantity     // increase the matching product quuantity +1
-      } else {                       // else push the product object(productId , quantity) in cart array
-        cart.push({
-          productId,         //productId: productId,
-          quantity             // quantity: quantity
-        })
-      }
-
-       
-      let cartQuantity = 0
-      cart.forEach((item)=>{
-        cartQuantity += item.quantity
-      })
-      
-      document.querySelector('.js-cart-quantity').innerHTML = cartQuantity
     })
   })
 
