db70723a20bd1a51177e37c9c08de7ebc42203a2
harishvenkateshan <harishvenkateshan@gmail.com>
2025-12-06 16:51:52 +0530

    convert products into classes

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,9 +1,9 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js"
 import {renderPaymentSummary} from "./checkout/paymentSummary.js"
-import dayjs from "https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js"
-import isSatSun from "../excercises/15th.js"
+// import dayjs from "https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js"
+// import isSatSun from "../excercises/15th.js"
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js"
-import '../data/cart-class.js'
+// import '../data/cart-class.js'
 
 renderOrderSummary()
 
